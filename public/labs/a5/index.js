a4729b456916e33ac4808b9df4767e22d91ff643
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-10-15 15:21:01 -0700

    a5 labs 2.7

--- public/labs/a5/index.js
+++ public/labs/a5/index.js
@@ -88,3 +88,71 @@ const twoSquared = square(2);
 const threePlusOne = plusOne(3);
 console.log(twoSquared);
 console.log(threePlusOne);
+
+// 2.7 arrays
+console.log('Arrays');
+let numberArray1 = [1, 2, 3, 4, 5];
+let stringArray1 = ['string1', 'string2'];
+let variableArray1 = [
+   functionScoped,
+   blockScoped,
+   constant1,
+   numberArray1,
+   stringArray1
+];
+console.log(numberArray1);
+console.log(stringArray1);
+console.log(variableArray1);
+
+console.log('Array index and length');
+const length1 = numberArray1.length;
+const index1 = numberArray1.indexOf(3);
+console.log(length1);
+console.log(index1);
+
+console.log('Add and remove data to arrays');
+// adding new items
+numberArray1.push(6);
+stringArray1.push('string3');
+// remove 1 item starting on 3rd spot
+numberArray1.splice(2, 1);
+stringArray1.splice(1, 1);
+console.log(numberArray1);
+console.log(stringArray1);
+
+console.log('For loops')
+for (let i=0; i<stringArray1.length; i++) {
+    const string1 = stringArray1[i];
+    console.log(string1);
+ }
+
+console.log('Map function');
+const squares = numberArray1.map(square);
+const cubes = numberArray1.map(a => a * a * a);
+console.log(squares);
+console.log(cubes);
+
+console.log('Find function');
+const four = numberArray1.find(a => a === 4);
+const string3 = stringArray1.find(a => a === 'string3');
+console.log(four);
+console.log(string3);
+
+console.log('Find index');
+const fourIndex = numberArray1
+   .findIndex(a => a === 4);
+const string3Index = stringArray1
+   .findIndex(a => a === 'string3');
+console.log(fourIndex);
+console.log(string3Index);
+
+console.log('Filter function');
+const numbersGreaterThan2 = numberArray1
+   .filter(a => a > 2);
+const evenNumbers = numberArray1
+   .filter(a => a % 2 === 0);
+const oddNumbers = numberArray1
+   .filter(a => a % 2 !== 0);
+console.log(numbersGreaterThan2);
+console.log(evenNumbers);
+console.log(oddNumbers);
