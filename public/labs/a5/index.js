18c45d5c9e4b0868ac918c2fbc8699f023aabfc6
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-10-15 17:23:50 -0700

    a5 labs 2.23

--- public/labs/a5/index.js
+++ public/labs/a5/index.js
@@ -252,6 +252,18 @@ const init = () => {
      }
      const eventTarget = $("#event-target");
      eventTarget.click(handleEventTarget);
-     
+    // 2.23
+    let hideBtn, showBtn, hideShowHeader;
+    hideBtn = $('#hide');
+    showBtn = $('#show');
+    hideShowHeader = $('#hide-show');
+    const hideHandler = () => {
+        hideShowHeader.hide();
+    }
+    const showHandler = () => {
+        hideShowHeader.show();
+    }
+    hideBtn.click(hideHandler);
+    showBtn.click(showHandler);
  }
  $(init);
