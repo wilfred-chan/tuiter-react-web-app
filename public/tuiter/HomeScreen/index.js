2290c36210ed03809fd2326d55fc14f196f32397
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-10-17 14:24:34 -0700

    a5 challenge replace WhoToFollow with PostSummaryList

--- public/tuiter/HomeScreen/index.js
+++ public/tuiter/HomeScreen/index.js
@@ -1,5 +1,5 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
-import WhoToFollowList from "../WhoToFollowList/index.js";
+import PostSummaryList from "../PostSummaryList/index.js";
 import PostList from "./PostList/PostList.js";
 
 function HomeComponent() {
@@ -12,7 +12,7 @@ function HomeComponent() {
         ${PostList()}
         </div>
         <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-         ${WhoToFollowList()}
+         ${PostSummaryList()}
         </div>
        </div>
     `);
