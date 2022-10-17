444a998ca089a4710185edf23d555af109b70dbd
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-10-16 18:39:39 -0700

    a5 challenge init

--- public/tuiter/HomeScreen/PostList/PostItem.js
+++ public/tuiter/HomeScreen/PostList/PostItem.js
@@ -1,27 +1,63 @@
 let post = {
   avatarIcon: "/public/images/avatar/nasa.jpg",
-  postBannger: "/public/images/paris.png",
+  banner: "/public/images/paris.png",
+  content:
+    "lorem ipsum dolor sit amet consectetur adipisicing elit. Similique harum quas aperiam tenetur incidunt doloribus porro iure tempora, ipsa inventore unde nobis iste ducimus repudiandae facere sit minima animi nisi.",
   userName: "NASA",
   handle: "@nasa",
 };
 
 const PostItem = (post) => {
   return `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-    <img
-    src="${psot.image}"
-    class="rounded-circle float-end"
-    style="width: 50px; height: 50px;"
-    />
-    <div class="ms-2 me-auto">
-        <div class="fw-bold">
-            ${who.userName}<i class="fas fa-check-circle mx-2"></i>
-        </div>
-        ${who.handle}
+    <div class="wd-bookmark">
+    <div class="wd-avatar">
+      <img
+        src="${post.avatarIcon}"
+        alt="profile pic 1"
+        height="48px"
+        width="48px"
+      />
     </div>
-    <button type="button" class="btn btn-primary rounded-pill">
-    Follow
-    </button>
-    </li>
-    `;
+    <div class="wd-content">
+      <div class="wd-username">
+        ${post.userName}
+        <span>${post.handle}</span>
+      </div>
+      <div class="wd-paragraph">
+      </div>
+      <img
+        class="wd-post-img"
+        src="${post.banner}"
+        alt="post images"
+        width="504px"
+        height="264px"
+      />
+      <div class="wd-post-img-textarea">
+        <div class="wd-post-img-title">This is a Title</div>
+        <div class="wd-post-img-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Similique harum quas aperiam tenetur incidunt doloribus porro
+          iure tempora, ipsa inventore unde nobis iste ducimus repudiandae
+          facere sit minima animi nisi.
+        </div>
+      </div>
+      <div class="wd-buttons">
+        <div>
+          <a href="#"><i class="far fa-comment"></i></a>
+        </div>
+        <div>
+          <a href="#"><i class="fas fa-retweet"></i> 9</a>
+        </div>
+        <div id="like">
+          <a href="#"><i class="far fa-heart"></i> 37</a>
+        </div>
+        <div>
+          <a href="#"><i class="fas fa-external-link-alt"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
 };
+
+$("#wd-test").append(PostItem(post));
