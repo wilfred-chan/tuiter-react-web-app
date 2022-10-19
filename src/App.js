2b9674d6e90e1c8768860d38deeb6827228f78e7
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-10-19 15:22:48 -0700

    a6 labs 2.3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Labs from "./labs";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="container">
+      <h1>Hello World!</h1>
+      <Labs />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/labs/index.js
@@ -0,0 +1,8 @@
+function Labs() {
+  return (
+    <div>
+      <h1>Assignment 6</h1>
+    </div>
+  );
+}
+export default Labs;
