ce5a2e30d3b164cb353d3dd92a5c10e6e8820a21
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-10-19 22:47:24 -0700

    a6 labs 2.9

--- src/labs/a6/hello-world.js
+++ src/labs/a6/hello-world.js
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
+import Nav from "../../nav";
 
 function HelloWorld() {
   return (
     <div>
-      <Link to="/">Lab</Link> | <Link to="/hello">Hello</Link> |
-      <Link to="/tuiter">Tuiter</Link>
+      <Nav />
       <h1>Hello World!</h1>
     </div>
   );
--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
+import Nav from "../../nav";
 
 function Assignment6() {
   return (
     <div>
-      <Link to="/">Lab</Link> | <Link to="/hello">Hello</Link> |
-      <Link to="/tuiter">Tuiter</Link>
+      <Nav />
       <h1>Assignment 6</h1>
     </div>
   );
