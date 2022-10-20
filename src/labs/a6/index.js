0bfe8c187bd2d9ed82490b8c207303d58cac3a37
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-10-20 09:52:58 -0700

    a6 labs 2.12

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 import Nav from "../../nav";
 import Classes from "./classes";
 import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
 
 function Assignment6() {
   return (
     <div>
       <Nav />
       <h1>Assignment 6</h1>
+      <ConditionalOutput />
       <Styles />
       <Classes />
     </div>
