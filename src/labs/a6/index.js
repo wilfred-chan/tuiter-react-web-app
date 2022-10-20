1f5f076b88e05ba23d696a2200c94f1bed4f2849
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-10-20 10:04:58 -0700

    a6 labs 2.13

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -3,12 +3,14 @@ import Nav from "../../nav";
 import Classes from "./classes";
 import Styles from "./styles";
 import ConditionalOutput from "./conditional-output";
+import TodoList from "./todo/todo-list";
 
 function Assignment6() {
   return (
     <div>
       <Nav />
       <h1>Assignment 6</h1>
+      <TodoList />
       <ConditionalOutput />
       <Styles />
       <Classes />
