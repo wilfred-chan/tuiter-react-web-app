992902ac4313c381c395a78537c82b6dd85bb9af
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-10-20 08:49:45 -0700

    a6 labs 2.10

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import Nav from "../../nav";
+import Classes from "./classes";
 
 function Assignment6() {
   return (
     <div>
       <Nav />
       <h1>Assignment 6</h1>
+      <Classes />
     </div>
   );
 }
