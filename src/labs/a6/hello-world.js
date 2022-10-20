5e227e08a18bd1bac8e8f27656f5c323b80756d6
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-10-19 22:37:37 -0700

    a6 labs 2.8

--- src/labs/a6/hello-world.js
+++ src/labs/a6/hello-world.js
@@ -1,4 +1,12 @@
+import { Link } from "react-router-dom";
+
 function HelloWorld() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div>
+      <Link to="/">Lab</Link> | <Link to="/hello">Hello</Link> |
+      <Link to="/tuiter">Tuiter</Link>
+      <h1>Hello World!</h1>
+    </div>
+  );
 }
 export default HelloWorld;
