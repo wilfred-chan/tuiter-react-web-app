a4956c15d42271d6910afd4119a1625d242cee36
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-10-19 18:09:00 -0700

    a6 labs 2.5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Labs from "./labs";
+import HelloWorld from "./labs/a6/hello-world";
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello World!</h1>
+      <HelloWorld />
       <Labs />
     </div>
   );
