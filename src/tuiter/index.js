64a1dbbd80532ed213631063c518eec31a6a0b5f
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-10-21 17:06:20 -0700

    a6 tuiter 3.2

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,12 +1,35 @@
 import { Link } from "react-router-dom";
 import Nav from "../nav";
 import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
 
 function Tuiter() {
   return (
     <div>
       <Nav />
       <NavigationSidebar active="home" />
+      <WhoToFollowListItem
+        who={{
+          userName: "NASA",
+          handle: "NASA",
+          avatarIcon: "nasa.jpg",
+        }}
+      />
+      <WhoToFollowListItem
+        who={{
+          userName: "Tesla",
+          handle: "tesla",
+          avatarIcon: "tesla.png",
+        }}
+      />
+      <WhoToFollowListItem
+        who={{
+          userName: "Relativity Space",
+          handle: "relativity_space",
+          avatarIcon: "relativity_space.jpg",
+        }}
+      />
+
       <h1>Tuiter</h1>
     </div>
   );
