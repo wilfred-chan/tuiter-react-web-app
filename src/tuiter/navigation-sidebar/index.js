fbee4589cce4442388726943d558d840b16997db
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-10-22 20:17:58 -0700

    a6 challenge 4.2

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,56 +1,65 @@
 import React from "react";
-const NavigationSidebar = ({ active = "explore" }) => {
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
+
+const NavigationSidebar = () => {
+  const { pathname } = useLocation();
+  const paths = pathname.split("/");
+  const active = paths[2];
+
   return (
     <div className="list-group">
       <a className="list-group-item">Tuiter</a>
-      <a
-        className={`list-group-item
-                   ${active === "home" ? "active" : ""}`}
+      <Link
+        to="/tuiter/home"
+        className={`list-group-item ${active === "home" ? "active" : ""}`}
       >
         Home
-      </a>
-      <a
-        className={`list-group-item
-                   ${active === "explore" ? "active" : ""}`}
+      </Link>
+      <Link
+        to="/tuiter/explore"
+        className={`list-group-item ${active === "explore" ? "active" : ""}`}
       >
         Explore
-      </a>
-      <a
-        className={`list-group-item
-                   ${active === "notifications" ? "active" : ""}`}
+      </Link>
+      <Link
+        to="/tuiter/notifications"
+        className={`list-group-item ${
+          active === "notifications" ? "active" : ""
+        }`}
       >
         Notifications
-      </a>
-      <a
-        className={`list-group-item
-                   ${active === "messages" ? "active" : ""}`}
+      </Link>
+      <Link
+        to="/tuiter/messages"
+        className={`list-group-item ${active === "messages" ? "active" : ""}`}
       >
         Messages
-      </a>
-      <a
-        className={`list-group-item
-                   ${active === "bookmarks" ? "active" : ""}`}
+      </Link>
+      <Link
+        to="/tuiter/bookmarks"
+        className={`list-group-item ${active === "bookmarks" ? "active" : ""}`}
       >
         Bookmarks
-      </a>
-      <a
-        className={`list-group-item
-                   ${active === "lists" ? "active" : ""}`}
+      </Link>
+      <Link
+        to="/tuiter/lists"
+        className={`list-group-item ${active === "lists" ? "active" : ""}`}
       >
         Lists
-      </a>
-      <a
-        className={`list-group-item
-                   ${active === "profile" ? "active" : ""}`}
+      </Link>
+      <Link
+        to="/tuiter/profile"
+        className={`list-group-item ${active === "profile" ? "active" : ""}`}
       >
         Profile
-      </a>
-      <a
-        className={`list-group-item
-                   ${active === "more" ? "active" : ""}`}
+      </Link>
+      <Link
+        to="/tuiter/more"
+        className={`list-group-item ${active === "more" ? "active" : ""}`}
       >
         More
-      </a>
+      </Link>
     </div>
   );
 };
