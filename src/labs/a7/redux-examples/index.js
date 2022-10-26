d2c594fcdf514eaf519a55b4d1dbd7ad7b5cf847
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-10-26 14:51:11 -0700

    a7 labs 2.4

--- src/labs/a7/redux-examples/index.js
+++ src/labs/a7/redux-examples/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import HelloReduxExampleComponent from "./hello-redux-example-component";
+import Hello from "./reducers/hello";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
+const store = createStore(Hello);
 const ReduxExamples = () => {
   return (
-    <div>
-      <h2>Redux Examples</h2>
-    </div>
+    <Provider store={store}>
+      <div>
+        <h2>Redux Examples</h2>
+        <HelloReduxExampleComponent />
+      </div>
+    </Provider>
   );
 };
 
