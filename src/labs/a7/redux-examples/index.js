8fd7ecfbe8f12160e4a2e37f6c466e0f26fcc06f
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-11-05 09:43:21 -0700

    a7 labs 2.5

--- src/labs/a7/redux-examples/index.js
+++ src/labs/a7/redux-examples/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import HelloReduxExampleComponent from "./hello-redux-example-component";
-import Hello from "./reducers/hello";
-import { createStore } from "redux";
+import hello from "./reducers/hello";
+import todos from "./reducers/todos-reducer";
+import { configureStore } from "@reduxjs/toolkit";
+
+import Todos from "./todos-component";
 import { Provider } from "react-redux";
 
-const store = createStore(Hello);
+const store = configureStore({ reducer: { hello, todos } });
 const ReduxExamples = () => {
   return (
     <Provider store={store}>
       <div>
         <h2>Redux Examples</h2>
+        <Todos />
         <HelloReduxExampleComponent />
       </div>
     </Provider>
