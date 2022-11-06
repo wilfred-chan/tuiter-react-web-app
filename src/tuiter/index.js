9d121a814ce76505a3fd7526f5f0328e385ad8ea
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-11-06 09:13:04 -0800

    a7 tuiter 3.2

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -5,10 +5,13 @@ import WhoToFollowList from "./who-to-follow-list";
 import HomeComponent from "./home";
 import { Routes, Route } from "react-router";
 import whoReducer from "./reducers/who-reducer";
+import tuitsReducer from "./reducers/tuits-reducer";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 
-const store = configureStore({ reducer: { who: whoReducer } });
+const store = configureStore({
+  reducer: { who: whoReducer, tuits: tuitsReducer },
+});
 
 function Tuiter() {
   return (
