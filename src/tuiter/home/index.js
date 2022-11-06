d4232d1a0b2fbd72ace0b4381a567dc03f62f2da
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-11-06 13:12:35 -0800

    a7 tuiter 3.3.2

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -1,9 +1,12 @@
 import tuits from "../tuits/tuits.json";
 import TuitItem from "../tuits/tuits-list/tuit-item";
+import WhatsHappening from "./whats-happening";
 
 const HomeComponent = () => {
   return (
     <>
+      <h4>Home</h4>
+      <WhatsHappening />
       <div>{tuits.map((tuit) => TuitItem({ tuit }))}</div>
     </>
   );
